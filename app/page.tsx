8136f494989ddedf7c67ec25d6dfa9956494fd44
joeulam <joeulam345@gmail.com>
2024-11-24 17:40:26 -0500

    Brutally minialistic

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,80 @@
-import Image from "next/image";
+'use client';
+import { Avatar, Box, Center, Container, Flex, MantineProvider, Affix} from '@mantine/core';
+import Typewriter from 'typewriter-effect';
+
+import '@mantine/core/styles.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <MantineProvider forceColorScheme="light">
+        <Box
+        style={{
+          position: 'sticky', // Makes the Box sticky
+          top: '5vh', // Sticks the Box to the top of the viewport
+          zIndex: 10, // Ensures the Box stays on top of other elements
+        }}>
+          <Flex
+            className='ml-10 mr-10 mt-10'
+            justify="space-between"
+          >
+            <Box>Joey</Box>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <Flex
+              gap={"10vw"}
+            >
+              <Box>About</Box>
+              <Box>Resume</Box>
+              <Box>Projects</Box>
+            </Flex>
+          </Flex>
+        </Box>
+      <Center
+      h={"90vh"}>
+        <Flex gap={40}>
+          <Avatar size={"15vw"} src="headshot.png" alt="it's me" />
+          <Center>
+            <Box>
+              <Box>
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter.typeString('Hi Im Joey Lam')
+                      .pauseFor(2500)                
+                      .start();
+                  }}
+                />
+              </Box>
+              <Flex gap={30}>
+                <Box w={"30vw"}>
+                  As of December 2024 I'm still currently looking for any internship, research, or mentorship. If you think I'm a good fit please reach out.
+                </Box>
+              </Flex>
+            </Box>
+          </Center>
+
+        </Flex>
+      </Center>
+      <Box h={"100vh"}>
+        About me
+        <Box>
+          Bello! My name is Joey! I'm a current sophmore at Boston University studying Computer Science & Economics. I'm intrested in optimizations, machine learning and finance. In my free time I like to take photos, video/photo editing and playing ping pong!  
+        </Box>
+      </Box>
+      <Box h={"100vh"}>
+        Resume
+        <Box>
+          This is my current resume!
+        </Box>
+        Last updated {"date"} 
+      </Box>
+      <Box h={"100vh"}>
+        Projects
+        <Box>
+          List of projects that I have done
+        </Box>
+      </Box>
+      <Box h={"100vh"}>
+        Others!
+      </Box>
+    </MantineProvider>
   );
 }
